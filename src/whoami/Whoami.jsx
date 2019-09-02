54b1b00b98c89d5e21f0c199f6f7821c2716200f
kurari <ismail.kurar@commencis.com>
2019-09-02 15:23:47 +0300

    add whoami

--- src/whoami/Whoami.jsx
+++ src/whoami/Whoami.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 
+import { Icon } from 'antd';
+
 import Page from '../components/page/Page';
 
 const Whoami = () => (
   <Page>
     <div className="whoami-container">
       <div className="title">
-        <div className="name">Ismail Kurar</div>
+        <div className="name">İsmail Kurar</div>
         <div className="job">Senior Frontend Engineer @Commencis</div>
+        <div className="contacts">
+          <a href="https://www.linkedin.com/in/ikurar/" target="_blank" rel="noopener noreferrer">
+            <Icon type="linkedin" className="linkedin"/>
+          </a>
+        </div>
       </div>
     </div>
   </Page>
